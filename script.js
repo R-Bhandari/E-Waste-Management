a987a88b7a0f28da2ec106758c1bfbe8a1f6bca6
ritikprajapati21 <ritikpfb21@svvv.edu.in>
2022-10-31 23:01:05 +0530

    Semifinal commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function mess() {
+    // document.querySelector("form").style.display = none;
+    document.getElementById("message").innerHTML = "We have got your details, our team will approach you with the confirmation mail on your registered mail. Also we will drop the message of Instructions on your registered mobile number for the further process";
+    // alert("We have got your details, our team will approach you with the confirmation mail on your registered mail. Also we will drop the message of Instructions on your registered mobile number for the further process");
+}
+// document.getElementById("submit1").onclick = message;
