f89ef33efa78e4700757568d59f9acb1bd80ba3a
Raghav <raghavbhandari082@gmail.com>
2022-11-01 16:26:18 +0530

    Finally updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function mess() {
-    // document.querySelector("form").style.display = none;
-    document.getElementById("message").innerHTML = "We have got your details, our team will approach you with the confirmation mail on your registered mail. Also we will drop the message of Instructions on your registered mobile number for the further process";
-    // alert("We have got your details, our team will approach you with the confirmation mail on your registered mail. Also we will drop the message of Instructions on your registered mobile number for the further process");
+function messageUpdate() {
+    // document.getElementById("message").innerHTML="We have got your details, our team will approach you with the confirmation mail on your registered mail. Also we will drop the message of Instructions on your registered mobile number for the further process"
+    confirm("We have got your details, our team will approach you with the confirmation mail on your registered mail.  \n Also we will drop the message of Instructions on your registered mobile number for the further process. \n Thank You!!");
 }
-// document.getElementById("submit1").onclick = message;
+function submitForm() {
+    confirm("Thank You for contacting us. Our team has got your message. \n We will get back to you as soon as possible. \n Thank You")
+}
